cd2341c35304279b5c988e66253955adb81ec425
GeraltZRivii <throughyourhead@gmail.com>
2016-11-18 20:42:50 +0300

    Fist subtask finished

--- Main.cpp
+++ Main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
 using std::vector;
 using std::string;
 
-// первое вхождение элемента в вектор
+// 1. первое вхождение элемента в вектор
 // return: индекс, либо size, если такого нет
-int find(const vector<string>, const string);
+int find(const vector<string>&, const string&);
 
 int main() {
     // 1. test find
-    
+    const string str1 = "el3", str2 = "el6";
+    const vector<string> vect1 = { "el1", "el2", "el2", "el3", "el4", "el3", "el5" };
+    cout << "Task 1:\n";
+    cout << find(vect1, str1) << endl;
+    cout << find(vect1, str2) << endl;
 
     return 0;
 }
