635cdb20b34e678a03431797da469c61aa9b4706
GeraltZRivii <throughyourhead@gmail.com>
2016-11-19 00:13:22 +0300

    Third subtask finished

--- Main.cpp
+++ Main.cpp
@@ -12,6 +12,9 @@ int find(const vector<string>&, const string&);
 // 2. заменить каждое вхождение элемента
 void replace(vector<string>&, const string&, const string&);
 
+// 3. удалить первое вхождение элемента в векторе
+template <typename T> void delete_elem(vector<T>&, const T&);
+
 int main() {
     // 1. test find
     string str1 = "el3", str2 = "el6";
@@ -29,21 +32,36 @@ int main() {
         cout << *it << ' ';
     cout << endl;
 
+    // 3. test delete
+    str1 = "el2";
+    vect1 = { "el1", "el2", "el2", "el3", "el1", "el4", "el3", "el1" };
+    cout << "\nTask 3:\n";
+    delete_elem(vect1, str1);
+    for (vector<string>::iterator it = vect1.begin(); it < vect1.end(); it++)
+        cout << *it << ' ';
+    cout << endl;
+
     return 0;
 }
 
 int find(const vector<string> &a_vector, const string &target) {
     int i = 0;
-    for (vector<string>::const_iterator it = a_vector.begin(); it < a_vector.end(); it++, i++) {
+    for (vector<string>::const_iterator it = a_vector.begin(); it < a_vector.end(); it++, i++)
         if ((*it).compare(target) == 0)
             return i;
-    }
     return a_vector.size();
 }
 
 void replace(vector<string> &a_vector, const string &old_item, const string &new_item) {
-    for (vector<string>::iterator it = a_vector.begin(); it < a_vector.end(); it++) {
+    for (vector<string>::iterator it = a_vector.begin(); it < a_vector.end(); it++)
         if ((*it).compare(old_item) == 0)
             *it = new_item;
-    }
+}
+
+template <typename T> void delete_elem(vector<T> &a_vector, const T &target) {
+    for (vector<string>::iterator it = a_vector.begin(); it < a_vector.end(); it++)
+        if ((*it) == target) {
+            a_vector.erase(it);
+            return;
+        }
 }
